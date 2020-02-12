9143ef94c1b3b0c055a6adec2f6b809a6deb5fc8
Nam <nam.quach2205@gmail.com>
2020-02-11 23:23:42 -0500

    added clear and reset tests

--- tests/test_input_system.py
+++ tests/test_input_system.py
@@ -26,6 +26,30 @@ class InputSystemTestCase(unittest.TestCase):
 
         self.assertEqual(in_sys.round, 1)
 
+    def test_reset(self):
+        in_sys = InputSystem()
+
+        in_sys.round = 100
+        old_sequence = in_sys.mastermind.gen_sequence
+
+        in_sys.reset()
+
+        self.assertEqual(in_sys.round, 1)
+        self.assertEqual(in_sys.game, 1)
+        self.assertIsNot(old_sequence, in_sys.mastermind.gen_sequence)
+
+    def test_clear(self):
+        in_sys = InputSystem()
+
+        in_sys.make_guess([1, 2, 3, 4])
+
+        self.assertEqual(len(in_sys.mastermind.current_history), 1)
+
+        in_sys.clear()
+
+        self.assertEqual(len(in_sys.mastermind.current_history), 0)
+        self.assertEqual(len(in_sys.mastermind.entire_history), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
