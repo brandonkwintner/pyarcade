6fa392b8bb60acafb08314c4eda42e4dd90d8c6c
Nam <nam.quach2205@gmail.com>
2020-02-11 23:16:17 -0500

    add make guess test function

--- tests/test_input_system.py
+++ tests/test_input_system.py
@@ -1,4 +1,5 @@
 from pyarcade.input_system import InputSystem
+from pyarcade.eval_input import Evaluation
 import unittest
 
 
@@ -9,3 +10,22 @@ class InputSystemTestCase(unittest.TestCase):
         self.assertEqual(in_sys.round, 1)
         self.assertEqual(in_sys.game, 1)
         self.assertIsNotNone(in_sys.mastermind)
+
+    def test_make_guess(self):
+        in_sys = InputSystem()
+
+        current_seq = in_sys.mastermind.gen_sequence
+        # purposely make an incorrect guess
+        guess = [x+1 for x in current_seq]
+
+        self.assertFalse(in_sys.make_guess(guess))
+
+        self.assertEqual(in_sys.game, 1)
+
+        self.assertTrue(in_sys.make_guess(current_seq))
+
+        self.assertEqual(in_sys.round, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
