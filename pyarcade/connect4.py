89625704a2b21b21b093448b94255d62e7efe9da
Nam <nam.quach2205@gmail.com>
2020-02-13 14:25:08 -0500

    made guess sequence input

--- pyarcade/connect4.py
+++ pyarcade/connect4.py
@@ -1,7 +1,10 @@
 from pyarcade.connect4_states import C4State
+from typing import List
 
 
 class Connect4:
+    MAX_ROWS = 6
+    MAX_COLS = 7
     """ A class representing a Connect4 game session
     """
     def __init__(self):
@@ -11,17 +14,17 @@ class Connect4:
         self.current_history = Connect4.setup_board()
 
     @staticmethod
-    def setup_board():
+    def setup_board() -> List[List[C4State]]:
         """ Sets up a blank connect 4 game.
 
             Returns:
-                6x7 Array of empty states
+                MAX_ROWS x MAX_COLS 2d array of empty states.
         """
         board = []
 
-        for _ in range(6):
+        for _ in range(Connect4.MAX_ROWS):
             # array of size 7 filled with empty states
-            row_array = [C4State.E] * 7
+            row_array = [C4State.E] * Connect4.MAX_COLS
 
             board.append(row_array)
 
@@ -39,3 +42,35 @@ class Connect4:
 
         self.entire_history = []
         self.current_history = Connect4.setup_board()
+
+    def get_turn(self) -> C4State:
+        """ Determines the turn of the 2 players.
+
+            Returns:
+                C4State.X if even
+                C4State.O if odd
+
+        """
+
+        return C4State.X if len(self.current_history) % 2 == 0 else C4State.O
+
+    def guess_sequence(self, row: int, col: int) -> bool:
+        """ Inputs a player's move.
+
+            Args:
+                col - x position, indexed at 0.
+                row - y position, indexed at 0.
+
+            Returns:
+                True if winning move was made.
+
+        """
+
+        if not isinstance(row, int) or not isinstance(col, int):
+            return False
+        elif row < 0 or col < 0:
+            return False
+        elif row >= Connect4.MAX_ROWS or col >= Connect4.MAX_COLS:
+            return False
+
+        self.current_history[row][col] = self.get_turn()
