1ed37755774fd2982a337a4476b9020c36a3d2f5
Nam <nam.quach2205@gmail.com>
2020-02-13 11:04:49 -0500

    made init for connect4

---
+++ pyarcade/connect4.py
@@ -0,0 +1,18 @@
+
+
+class Connect4:
+    """ A class representing a Connect4 game session
+    """
+    def __init__(self):
+        # all previous games
+        self.entire_history = []
+        # current 6x7 board
+        self.current_history = []
+
+    def setup_board(self):
+        """ Sets up a blank connect 4 game.
+
+            Returns:
+                6x7 Array of empty states
+        """
+        return []
