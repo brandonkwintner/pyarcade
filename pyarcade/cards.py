fd7f35a335101ba07b957083fbea8ab894bc798d
Andy <Andyzh21@gmail.com>
2020-02-20 19:18:15 -0500

    Added Andy's code

---
+++ pyarcade/cards.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class Ranks(Enum):
+    One = 1
+    Two = 2
+    Three = 3
+    Four = 4
+    Five = 5
+    Six = 6
+    Seven = 7
+    Eight = 8
+    Nine = 9
+    Ten = 10
+    Jack = 10.1
+    Queen = 10.2
+    King = 10.3
+    Ace = 11
+
+
+class Suits(Enum):
+    Diamonds = "Diamonds"
+    Clubs = "Clubs"
+    Hearts = "Hearts"
+    Spades = "Spades"
+
+
