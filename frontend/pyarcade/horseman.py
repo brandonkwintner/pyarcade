5f33459f83a954734579f9e028ba797accfd5fc2
Ryan Ziemski <rziemski@terpmail.umd.edu>
2020-04-20 22:03:08 -0400

    worked on checking user guess

--- frontend/pyarcade/horseman.py
+++ frontend/pyarcade/horseman.py
@@ -1,6 +1,6 @@
 from pyarcade.abstract_game import AbstractGame
 from pyarcade.difficulties import Difficulty
-from typing import Optional
+from typing import Optional, List
 
 
 class Horseman(AbstractGame):
@@ -12,28 +12,68 @@ class Horseman(AbstractGame):
         else:
             self.word_length = 8
 
-        self.num_guesses = 0
+        self.num_guesses_left = 6
         self.difficulty = difficulty
         self.word = self.pick_word()
+        self.current_word = ["_" for i in range(len(self.word))]
+        self.game_over = False
 
     @staticmethod
-    def pick_word():
+    def pick_word() -> List[str]:
         return ""
 
     def enter_user_turn(self, guess) -> bool:
-        """ Checks if guess is correct in some way according
-        to the current game.
+        """ Checks if letter is in the word.
 
         Args:
-            guess: User's to the game.
+            guess: User's guessed letter.
 
         Returns:
-            result (bool): True if correct, false otherwise.
+            result (bool): True if game is over, false otherwise.
 
         """
         super().enter_user_turn(guess)
 
+        result = self.check_letter(guess)
 
+        if self.game_over and result:
+            self.entire_history.append("Player won", self.current_word)
+            self.current_history.append("Player won", self.current_word)
+            return True
+        elif self.game_over and not result:
+            self.entire_history.append("Player lost", self.word)
+            self.current_history.append("Player lost", self.word)
+            return True
+        else:
+            return False
+
+    def check_letter(self, guessed_letter: str) -> bool:
+        letter_found = False
+
+        for letter in self.word:
+            if guessed_letter == letter:
+                letter_found = True
+                self.current_word[self.word.index(letter)] = letter
+
+        if letter_found is False:
+            self.num_guesses_left -= 1
+
+            if self.num_guesses_left == 0:
+                self.game_over = True
+
+            return False
+
+        if self.check_for_winner():
+            self.game_over = True
+
+        return True
+
+    def check_for_winner(self) -> bool:
+        for letter in self.current_word:
+            if letter == "_":
+                return False
+
+        return True
 
     def reset_game(self):
         """ Reset single game. (input reset)
@@ -53,12 +93,14 @@ class Horseman(AbstractGame):
         self.word = self.pick_word()
 
     def get_last_turn(self):
-        """ Gets the last turn/state of game.
+        """ Gets the last turn/state of game (the amount of the word guessed).
 
         Returns:
             Meaningful state of the current game.
         """
-        pass
+        super.get_last_turn()
+
+        return ''.join(self.current_word), self.game_over
 
     @staticmethod
     def get_regex_pattern() -> str:
