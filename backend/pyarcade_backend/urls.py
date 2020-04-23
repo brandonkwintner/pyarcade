cc8eee8ee8df2838529e0c8df7fe714389c20b34
Brandon Kwintner <kwintner@terpmail.umd.edu>
2020-04-23 17:00:57 -0400

    API GET for games played

--- backend/pyarcade_backend/urls.py
+++ backend/pyarcade_backend/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from .views.users_views import SignUpView
 from .views.users_views import LogInView
 from .views.stats_views import UserStatsView
-
+from .views.games_played_views import GamesPlayedView
 
 urlpatterns = [
     path("users/signup/", SignUpView.as_view(), name="signup"),
     path("users/login/", LogInView.as_view(), name="login"),
     path("stats/", UserStatsView.as_view(), name="userstats"),
+    path("games_played/", GamesPlayedView.as_view(),
+         name="gamesplayedall")
+    # path("games_played?game={game}/", GamesPlayedView.as_view(),
+    #     name="gamesplayed"),
 ]
