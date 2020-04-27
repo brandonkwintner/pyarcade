bb710193c6e5087fb2d084b637672b557b0a2c61
Ryan Ziemski <rziemski@terpmail.umd.edu>
2020-04-26 20:31:04 -0400

    added game_wins views

--- backend/pyarcade_backend/urls.py
+++ backend/pyarcade_backend/urls.py
@@ -4,11 +4,13 @@ from .views.users_views import SignUpView
 from .views.users_views import LogInView
 from .views.stats_views import ResetUserStatsView
 from .views.games_played_views import GamesPlayedView
+from .views.game_wins_views import GameWinsView
 
 urlpatterns = [
     path("users/signup/", SignUpView.as_view(), name="signup"),
     path("users/login/", LogInView.as_view(), name="login"),
     path("stats/reset/", ResetUserStatsView.as_view(), name="reset_stats"),
     path("games_played/", GamesPlayedView.as_view(),
-         name="gamesplayed")
+         name="gamesplayed"),
+    path("game_wins/", GameWinsView.as_view(), name="game_wins")
 ]
